b39b26e2cb31a75a62cf4ff72bf649dff1b8f02e
Sahil <sahilkhemariya327@gmail.com>
2025-03-08 15:27:58 +0530

    [Sahil] Add. Person_Model

---
+++ address-book/src/app/models/person.model.ts
@@ -0,0 +1,10 @@
+export interface Person {
+    id?: number;
+    fullName: string;
+    address: string;
+    city: string;
+    state: string;
+    zipCode: string;
+    phoneNumber: string;
+  }
+  
