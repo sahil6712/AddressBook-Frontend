c0cf68ef27e579a971067a38055bae2c394148ab
Sahil <sahilkhemariya327@gmail.com>
2025-03-08 15:26:49 +0530

    [Sahil] Add. Person model

---
+++ address-book/src/app/components/person-form/person-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-form',
+  imports: [],
+  templateUrl: './person-form.component.html',
+  styleUrl: './person-form.component.scss'
+})
+export class PersonFormComponent {
+
+}
---
+++ address-book/src/app/components/person-list/person-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-list',
+  imports: [],
+  templateUrl: './person-list.component.html',
+  styleUrl: './person-list.component.scss'
+})
+export class PersonListComponent {
+
+}
