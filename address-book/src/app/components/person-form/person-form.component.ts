811ddf756ea20c9b3162e42ec0ec00fd5a23f9bd
Sahil <sahilkhemariya327@gmail.com>
2025-03-08 15:33:17 +0530

    [Sahil] Refactor. Person form

--- address-book/src/app/components/person-form/person-form.component.ts
+++ address-book/src/app/components/person-form/person-form.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { PersonService } from '../../services/person.service';
+import { Person } from '../../models/person.model';
 
 @Component({
   selector: 'app-person-form',
-  imports: [],
+  standalone: true,
   templateUrl: './person-form.component.html',
-  styleUrl: './person-form.component.scss'
+  styleUrl: './person-form.component.css'
 })
 export class PersonFormComponent {
+  person: Person = { fullName: '', address: '', city: '', state: '', zipCode: '', phoneNumber: '' };
 
+  constructor(private personService: PersonService) {}
+
+  addPerson() {
+    this.personService.addPerson(this.person).subscribe(() => {
+      alert('Person added successfully');
+      this.person = { fullName: '', address: '', city: '', state: '', zipCode: '', phoneNumber: '' };
+    });
+  }
 }
